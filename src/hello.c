cd8c639792c7d857d4189a4f9a7fd9c6c989bed4
brian.orwe <brian.orwe@gmail.com>
2024-12-12 00:37:38 +0300

    Running build shows instructions for executing days

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-  printf("Hello from Advent Of Code 2024");
+  printf("Hello from Advent Of Code 2024\n");
+  printf("After building use:\n");
+  printf("\tcmake --build ./build_dir -t day1 \n");
+  printf("\tTo run the first challenge of a day, in this case assuming the day is [1]\n");
+  printf("\tTo run second part of day do day1.5, the .5 meaning the second part, and the day in this case 1\n");
+  printf("\tcmake --build ./build_dir -t day1.5 \n");
   return 0;
 }
