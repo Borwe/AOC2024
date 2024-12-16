10cc1c2ea381133cd39a5433f804557e228445ef
brian.orwe <brian.orwe@gmail.com>
2024-12-16 15:47:26 +0300

    Done day3 part 1

---
+++ src/day3.c
@@ -0,0 +1,71 @@
+#include<ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"vec_utils.h"
+
+int get_mults(char *line, ulong width){
+  int result =0;
+  static const char *mul = "mul(";
+  Vec *numchar1 = vec_new_typed(char, 3);
+  Vec *numchar2= vec_new_typed(char, 3);
+
+  for(uint i = 0; i< width; i++){
+    if(memcmp(line+i,mul,4) == 0){
+      i+=4;
+      bool comma = false;
+      for(int j = i;j<width; j++,i=j){
+        if(*(line+j)==','){
+          comma = true;
+          continue;
+        }
+        if(isdigit(*(line+j))){
+          if(comma){
+            vec_add(numchar2,line+j);
+          }else{
+            vec_add(numchar1,line+j);
+          }
+          continue;
+        }
+        //break, shouldn't reach here
+        break;
+      }
+      if(*(line+i)==')' && numchar1->size <= 3 && numchar2->size <= 3){
+        int left = atoi(vec_get_mem(numchar1,char));
+        int right = atoi(vec_get_mem(numchar2,char));
+        //printf("got: %d, %d\n",left,right);
+        result=result + (left*right);
+      }
+    }
+    vec_clean(numchar1);
+    vec_clean(numchar2);
+  }
+  vec_free(numchar1);
+  vec_free(numchar2);
+  return result;
+}
+
+int main(int argc, char **argv){
+  FILE *f = fopen(argv[1], "r");
+  if(f==NULL){
+    char msg[512];
+    int size = snprintf(msg,512,"Couldn't open %s\n", argv[1]);
+    perror(msg);
+    return -1;
+  }
+
+  char *line = NULL;
+  ulong width = 1024 * 1024 * 1024;
+  int read = -1;
+
+  int sum = 0;
+  while((read = getline(&line, &width, f)) && read != -1){
+    sum += get_mults(line,read);
+    //printf("LINE: %s",line);
+  }
+
+  printf("SUMS: %d\n",sum);
+  fclose(f);
+
+  return 0;
+}
